b00b6c163a991ba4d832c3828166449472eb3cb0
Ricardo Rodrigues <nb25270@novabase.pt>
2022-07-28 23:01:42 +0100

    Updated project to use React18. Removed unused packages. Added prettier.

--- js/examples/src/DataMap.jsx
+++ js/examples/src/DataMap.jsx
@@ -1,41 +1,24 @@
-import React from 'react'
-import {Map, TileLayer} from 'react-leaflet'
-import ReactLeafletMultiOptionsPolyline from '../../'
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import ReactLeafletMultiOptionsPolyline from '../../';
 
-export default class DataMap extends React.Component {
-  constructor (props) {
-    super(props)
-    this.multiOptionsPolyline = null
-    this.ref = map => {
-      this.multiOptionsPolyline = map
-      this.setState({bounds: map.getBounds()})
-    }
-    this.state = {bounds: undefined}
-  }
-
-  render () {
+export default function DataMap({ positions, optionIdxFn, options }) {
     return (
-      <Map
-        bounds={this.state.bounds}
-        style={{height: '40%'}} >
-        <TileLayer
-          attribution={'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="http://mapbox.com">Mapbox</a>'}
-          url='https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5hcGFsdmVzIiwiYSI6ImNpcHIxcDRzajAwNzJpZW5idHNucnZjY2gifQ.vMgJ5qWfBiKa_R-hmuuO0w'
-        />
-        <ReactLeafletMultiOptionsPolyline
-          ref={this.ref}
-          positions={this.props.positions}
-          optionIdxFn={this.props.optionIdxFn}
-          options={this.props.options}
-          weight={5}
-          lineCap='butt'
-          opacity={0.75}
-          smoothFactor={1}
-          zoomAnimation={false}
-        />
-      </Map>
-    )
-  }
+        <MapContainer style={{ height: '40%' }}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+            />
+            <ReactLeafletMultiOptionsPolyline
+                positions={positions}
+                optionIdxFn={optionIdxFn}
+                options={options}
+                weight={5}
+                lineCap="butt"
+                opacity={0.75}
+                smoothFactor={1}
+                zoomAnimation={false}
+            />
+        </MapContainer>
+    );
 }
--- js/index.jsx
+++ js/index.jsx
@@ -1,95 +1,95 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {FeatureGroup, Polyline} from 'react-leaflet'
-import L from 'leaflet'
-import {getBounds} from 'geolib'
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { FeatureGroup } from 'react-leaflet';
+import { Polyline } from 'react-leaflet';
+import L from 'leaflet';
+import { getBounds } from 'geolib';
+import { useMap } from 'react-leaflet/hooks';
 
-export default class ReactLeafletMultiOptionsPolyline extends React.Component {
-  constructor (props) {
-    super(props)
-    this.mapPositions = this.mapPositions.bind(this)
-    this.state = {
-      lines: this.mapPositions()
-    }
-    this.getBounds = this.getBounds.bind(this)
-  }
-
-  mapPositions () {
-    const {positions, optionIdxFn, options} = this.props
-    const lines = new Array(options.length)
-    const currentLine = new Array(options.length)
-    for (let idx = 0; idx < options.length; ++idx) {
-      currentLine[idx] = []
-      lines[idx] = []
-    }
-    let last
-    let lastLineIdx = 0
-    let currentLineIdx = 0
-    for (let idx = 1; idx < positions.length; ++idx) {
-      const item = positions[idx]
-      const previous = positions[idx - 1]
-      const coordinates = new L.LatLng(item.lat, item.lng)
-      currentLineIdx = optionIdxFn(item, previous, idx, positions)
-      currentLine[currentLineIdx].push(coordinates)
-      // If 1st item, must handle item at index 0;
-      if (idx === 1) {
-        last = previous
-        lastLineIdx = optionIdxFn(previous, previous, idx - 1, positions)
-        currentLine[lastLineIdx].push(last)
-        if (lastLineIdx !== currentLineIdx) {
-          currentLine[lastLineIdx].push(coordinates)
-          lines[lastLineIdx].push(currentLine[lastLineIdx])
-          currentLine[lastLineIdx] = []
+export default function ReactLeafletMultiOptionsPolyline(props) {
+    const mapPositions = () => {
+        const { positions, optionIdxFn, options } = props;
+        const lines = new Array(options.length);
+        const currentLine = new Array(options.length);
+        for (let idx = 0; idx < options.length; ++idx) {
+            currentLine[idx] = [];
+            lines[idx] = [];
         }
-      } else {
-        // If the result of optionIdxFn from last is different from current, push last line and clear it;
-        if (lastLineIdx !== currentLineIdx) {
-          currentLine[lastLineIdx].push(coordinates)
-          lines[lastLineIdx].push(currentLine[lastLineIdx])
-          currentLine[lastLineIdx] = []
+        let last;
+        let lastLineIdx = 0;
+        let currentLineIdx = 0;
+        for (let idx = 1; idx < positions.length; ++idx) {
+            const item = positions[idx];
+            const previous = positions[idx - 1];
+            const coordinates = new L.LatLng(item.lat, item.lng);
+            currentLineIdx = optionIdxFn(item, previous, idx, positions);
+            currentLine[currentLineIdx].push(coordinates);
+            // If 1st item, must handle item at index 0;
+            if (idx === 1) {
+                last = previous;
+                lastLineIdx = optionIdxFn(
+                    previous,
+                    previous,
+                    idx - 1,
+                    positions
+                );
+                currentLine[lastLineIdx].push(last);
+                if (lastLineIdx !== currentLineIdx) {
+                    currentLine[lastLineIdx].push(coordinates);
+                    lines[lastLineIdx].push(currentLine[lastLineIdx]);
+                    currentLine[lastLineIdx] = [];
+                }
+            } else {
+                // If the result of optionIdxFn from last is different from current, push last line and clear it;
+                if (lastLineIdx !== currentLineIdx) {
+                    currentLine[lastLineIdx].push(coordinates);
+                    lines[lastLineIdx].push(currentLine[lastLineIdx]);
+                    currentLine[lastLineIdx] = [];
+                }
+            }
+            // Keep index and item
+            lastLineIdx = currentLineIdx;
+            last = item;
         }
-      }
-      // Keep index and item
-      lastLineIdx = currentLineIdx
-      last = item
-    }
-    // Push last items;
-    lines[currentLineIdx].push(currentLine[currentLineIdx])
-    return lines
-  }
+        // Push last items;
+        lines[currentLineIdx].push(currentLine[currentLineIdx]);
+        return lines;
+    };
+    const [lines, setLines] = useState(mapPositions());
+    const map = useMap();
 
-  shouldComponentUpdate (nextProps, nextState) {
-    return nextProps !== this.props
-  }
+    useEffect(() => {
+        const bounds = getBounds(props.positions);
+        map.fitBounds(
+            new L.LatLngBounds(
+                new L.LatLng(bounds.maxLat, bounds.minLng),
+                new L.LatLng(bounds.minLat, bounds.maxLng)
+            )
+        );
+    });
 
-  componentDidUpdate (prevProps, prevState, snapshot) {
-    this.setState({
-      lines: this.mapPositions()
-    })
-  }
+    useEffect(() => {
+        setLines(mapPositions());
+    }, [props.positions, props.optionIdxFn, props.options]);
 
-  getBounds () {
-    const bounds = getBounds(this.props.positions)
-    return new L.LatLngBounds(new L.LatLng(bounds.maxLat, bounds.minLng), new L.LatLng(bounds.minLat, bounds.maxLng))
-  }
-
-  render () {
     return (
-      <FeatureGroup>
-        {this.state.lines.map((optionsLine, optionsIdx) =>
-          optionsLine.map((line, idx) =>
-            <Polyline key={idx} {...this.props} positions={line} {...this.props.options[optionsIdx]} />
-          )
-        )}
-      </FeatureGroup>
-    )
-  }
+        <FeatureGroup>
+            {lines.map((optionsLine, optionsIdx) =>
+                optionsLine.map((line, idx) => (
+                    <Polyline
+                        key={idx}
+                        {...props}
+                        positions={line}
+                        {...props.options[optionsIdx]}
+                    />
+                ))
+            )}
+        </FeatureGroup>
+    );
 }
 
 ReactLeafletMultiOptionsPolyline.propTypes = {
-  positions: PropTypes.arrayOf(PropTypes.instanceOf(L.LatLng)).isRequired,
-  optionIdxFn: PropTypes.func.isRequired,
-  options: PropTypes.arrayOf(
-    PropTypes.object.isRequired
-  )
-}
+    positions: PropTypes.arrayOf(PropTypes.instanceOf(L.LatLng)).isRequired,
+    optionIdxFn: PropTypes.func.isRequired,
+    options: PropTypes.arrayOf(PropTypes.object.isRequired),
+};
